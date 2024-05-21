54933d18aec1d0a37bc21544852787c0c2d23307
Fábio Ferreira <martinhoferreira10@gmail.com>
2024-05-21 09:11:42 +0100

    Add fields to deals list response

--- api/src/deals/dto/listDealsResponse.dto.ts
+++ api/src/deals/dto/listDealsResponse.dto.ts
@@ -1,59 +1,121 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Expose } from 'class-transformer';
-import { IsDate, IsNumber, IsString } from 'class-validator';
+
+import { Deal, DealStatus, Milestone } from '../deals.entities';
 
 export class ListDealDtoResponse {
-  constructor(res: ListDealDtoResponse) {
+  constructor(res: Partial<Deal>) {
     Object.assign(this, res);
   }
 
   @ApiProperty()
-  @IsString()
   @Expose()
   id: string;
 
   @ApiProperty()
-  @IsString()
   @Expose()
   name: string;
 
   @ApiProperty()
-  @IsString()
   @Expose()
   description: string;
 
   @ApiProperty()
-  @IsDate()
+  @Expose()
+  carbonFootprint: string;
+
+  // shipping properties
+
+  @ApiProperty()
+  @Expose()
+  contractId: string;
+
+  @ApiProperty()
+  @Expose()
+  origin: string;
+
+  @ApiProperty()
+  @Expose()
+  destination: string;
+
+  @ApiProperty()
+  @Expose()
+  portOfOrigin: string;
+
+  @ApiProperty()
+  @Expose()
+  portOfDestination: string;
+
+  @ApiProperty()
+  @Expose()
+  totalValue: number;
+
+  @ApiProperty()
+  @Expose()
+  transport: Date;
+
+  @ApiProperty()
   @Expose()
   shippingStartDate: Date;
 
   @ApiProperty()
-  @IsDate()
   @Expose()
   expectedShippingEndDate: Date;
 
   @ApiProperty()
-  @IsNumber()
+  @Expose()
+  daysLeft: number;
+
+  @ApiProperty()
+  @Expose()
+  duration: number;
+
+  // state properties
+  @ApiProperty()
+  @Expose()
+  currentMilestone: string;
+
+  @ApiProperty({ type: Milestone, isArray: true })
+  @Expose()
+  milestones: Milestone[];
+
+  @ApiProperty({ enum: DealStatus })
+  @Expose()
+  status: DealStatus;
+
+  @ApiProperty()
+  @Expose()
+  buyerConfirmed: boolean;
+
+  @ApiProperty()
+  @Expose()
+  supplierConfirmed: boolean;
+
+  // financial properties
+
+  @ApiProperty()
   @Expose()
   investmentAmount: number;
 
   @ApiProperty()
-  @IsNumber()
   @Expose()
   revenue: number;
 
   @ApiProperty()
-  @IsNumber()
   @Expose()
   netBalance: number;
 
   @ApiProperty()
-  @IsNumber()
   @Expose()
   roi: number;
 
+  // ownership properties
+
   @ApiProperty()
-  @IsString()
   @Expose()
-  carbonFootprint: string;
+  proposalBuyerEmail: string;
+
+  @ApiProperty()
+  @Expose()
+  proposalSupplierEmail: string;
 }
--- api/test/listDeals.e2e-spec.ts
+++ api/test/listDeals.e2e-spec.ts
@@ -1,8 +1,10 @@
+import exp from 'constants';
+
 import { AccountType } from '@/users/users.model';
 
 import { TestApp } from './utils';
 
-describe('Misc Routes (e2e)', () => {
+describe('List deals (e2e)', () => {
   let app: TestApp;
 
   beforeEach(async () => {
@@ -14,7 +16,82 @@ describe('Misc Routes (e2e)', () => {
     await app.teardown();
   });
 
-  it('list user deals', async () => {
+  it('list deals details', async () => {
+    const { buyerToken } = await app.setupDeal();
+
+    const dealsReq = await app
+      .request()
+      .get('/deals')
+      .set('Authorization', `Bearer ${buyerToken}`)
+      .expect(200);
+
+    expect(dealsReq.body).toHaveLength(1);
+    expect(dealsReq.body[0].id).toBeDefined();
+    expect(dealsReq.body[0].shippingStartDate).toBeDefined();
+    expect(dealsReq.body[0].proposalBuyerEmail).toBeDefined();
+    expect(dealsReq.body[0].proposalSupplierEmail).toBeDefined();
+    expect(dealsReq.body[0].expectedShippingEndDate).toBeDefined();
+
+    expect(dealsReq.body[0]).toMatchObject({
+      buyerConfirmed: true,
+      contractId: 1,
+      currentMilestone: 0,
+      daysLeft: -1,
+      description: 'test',
+      destination: 'US',
+      duration: '0 weeks',
+      investmentAmount: 1,
+      milestones: [
+        {
+          description: 'Description of Milestone 1',
+          docs: [],
+          fundsDistribution: 20,
+        },
+        {
+          description: 'Description of Milestone 2',
+          docs: [],
+          fundsDistribution: 50,
+        },
+        {
+          description: 'Description of Milestone 3',
+          docs: [],
+          fundsDistribution: 0,
+        },
+        {
+          description: 'Description of Milestone 4',
+          docs: [],
+          fundsDistribution: 0,
+        },
+        {
+          description: 'Description of Milestone 5',
+          docs: [],
+          fundsDistribution: 0,
+        },
+        {
+          description: 'Description of Milestone 6',
+          docs: [],
+          fundsDistribution: 0,
+        },
+        {
+          description: 'Description of Milestone 7',
+          docs: [],
+          fundsDistribution: 30,
+        },
+      ],
+      name: 'test',
+      netBalance: 1,
+      origin: 'CN',
+      portOfDestination: 'US',
+      portOfOrigin: 'CN',
+      revenue: 1,
+      roi: 1,
+      status: 'confirmed',
+      supplierConfirmed: true,
+      totalValue: 1,
+    });
+  });
+
+  it('list user deals by status', async () => {
     const buyer = await app.createUser({
       accountType: AccountType.Buyer,
     });
