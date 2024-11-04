3f3773ea602749a16030ef282f0dbf6a2c2699bc
Fábio Ferreira <martinhoferreira10@gmail.com>
2024-11-04 13:28:49 +0000

    publishing is behind feature flag

--- web/src/components/dashboard/shipment-details/shipment-details-header/index.tsx
+++ web/src/components/dashboard/shipment-details/shipment-details-header/index.tsx
@@ -1,5 +1,5 @@
 import { CheckCircle } from "@phosphor-icons/react";
-import React from "react";
+import React, { useEffect } from "react";
 import { useModal } from "src/context/modal-context";
 
 interface ShipmentDetailsHeaderProps {
@@ -15,28 +15,38 @@ const ShipmentDetailsHeader: React.FC<ShipmentDetailsHeaderProps> = ({
   publish,
   isPublished,
 }) => {
+  const [publishEnabled, setPublishEnabled] = React.useState(false);
+
+  useEffect(() => {
+    const key = localStorage.getItem("publishEnabled");
+    if (key) {
+      setPublishEnabled(true);
+    }
+  }, []);
+
   return (
     <div className="px-[30px]">
       <div className="flex items-center gap-[15px] text-[26px]  leading-[1.2em] tracking-normal text-tm-black-80">
         <h1 className="font-bold">{productName}</h1>
         {/* <p className="font-light">#{shipmentNumber}</p> */}
-        {isPublished ? (
-          <p className="flex items-center text-sm font-light text-tm-green">
-            Published
-            <span className="ml-2">
-              <CheckCircle size={16} />
-            </span>
-          </p>
-        ) : (
-          <button
-            onClick={() => {
-              publish();
-            }}
-            className="rounded-md bg-tm-green px-2 py-1 text-sm text-tm-white"
-          >
-            Publish
-          </button>
-        )}
+        {publishEnabled &&
+          (isPublished ? (
+            <p className="flex items-center text-sm font-light text-tm-green">
+              Published
+              <span className="ml-2">
+                <CheckCircle size={16} />
+              </span>
+            </p>
+          ) : (
+            <button
+              onClick={() => {
+                publish();
+              }}
+              className="rounded-md bg-tm-green px-2 py-1 text-sm text-tm-white"
+            >
+              Publish
+            </button>
+          ))}
       </div>
     </div>
   );
